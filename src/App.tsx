41a5c1500465a235ef820cde7995c3c16dcef3db
Tran.Luc <anhvanluc1999@gmail.com>
2023-12-11 23:20:19 +0700

    create UI home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 import { AppRoutes } from "./routes";
 
+const GlobalStyle = createGlobalStyle`
+  *, body {
+    padding: 0;
+    margin: 0;
+    font-family: Inter;
+  }
+`;
+
 export const App = () => {
-    return <AppRoutes />;
+    return (
+        <>
+            <GlobalStyle />
+            <AppRoutes />
+        </>
+    );
 };
